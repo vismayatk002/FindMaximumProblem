5e1bd7f90facfa70f13e7c890ee18d98b0b071fe
vismayatk002 <vismayatk002@gmail.com>
2021-08-10 19:32:26 +0530

    changed variable name

--- src/main/java/com/findmaximumnumber/FindMaximum.java
+++ src/main/java/com/findmaximumnumber/FindMaximum.java
@@ -8,25 +8,25 @@ public class FindMaximum implements MaxOperation
         Integer intObj1 = Integer.valueOf("800");
         Integer intObj2 = Integer.valueOf("557");
         Integer intObj3 = Integer.valueOf("500");
-        int maxIntValue = find.findMaxInt(intObj1, intObj2, intObj3);
-        System.out.println("Integer Maximum : " + maxIntValue);
+        int maxValue = find.findMaxInt(intObj1, intObj2, intObj3);
+        System.out.println("Integer Maximum : " + maxValue);
     }
     public int findMaxInt(Integer intObj1, Integer intObj2, Integer intObj3) {
     	
-    	int maxIntValue;
+    	int maxValue;
     	int result1 =  intObj1.compareTo(intObj2);
     	int result2 =  intObj1.compareTo(intObj3);
     	int result3 =  intObj2.compareTo(intObj3);
     	if(result1 > 0 && result2 > 0) {
-    		maxIntValue = intObj1;
+    		maxValue = intObj1;
     	}
     	else if(result3 > 0 && result1 < 0) {
-    		maxIntValue = intObj2;
+    		maxValue = intObj2;
     	}
     	else{
-    		maxIntValue = intObj3;
+    		maxValue = intObj3;
     	}
-    	return maxIntValue;
+    	return maxValue;
     }
 }
 
